24145bcb18e9fc62f6aa82cf5b6b0da6169abeb7
darkun7 <46642970+darkun7@users.noreply.github.com>
2020-12-20 06:51:24 +0700

    update: mapping route based on user privilage

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,39 @@ Route::get('/test', function () {
 
 Route::group(['as' => 'front.'], function () {
     Route::get('/', 'FrontController@index')->name('index');
+    Route::get('/dasbor', 'FrontController@dasbor')->name('dasbor')->middleware('auth');
+});
+Route::get('/pengumuman/{id}/show','PengumumanController@show')->name('pengumuman.show');
 
-    Route::get('/dasbor', 'FrontController@dasbor')->name('dasbor')->middleware('auth'); //dikasih middleware auth
-});
 Auth::routes();
-Route::group(['middleware' => ['auth','CekRole:admin']],function(){
-Route::resource('akun',"AkunController");
-Route::resource('fakultas',"FakultasController");
-Route::resource('jadwal',"JadwalController");
-Route::resource('lomba',"LombaController");
-Route::resource('pengumpulan',"PengumpulanController");
-Route::resource('pengumuman',"PengumumanController");
-Route::resource('submisi',"SubmisiController");
-Route::resource('tim',"TimController");
+Route::resource('akun',"AkunController")->middleware('auth');
+Route::get('/jadwal', 'JadwalController@index')->name('jadwal.index')->middleware('auth');
+Route::get('/pengumuman', 'PengumumanController@index')->name('pengumuman.index')->middleware('auth');
+// JURI //
+Route::resource('submisi',"SubmisiController")->middleware(['auth', 'CekRole:juri']);
+// ADMIN //
+Route::group(['middleware' => ['auth','CekRole:peserta']],function(){
+  Route::resource('tim',"TimController");
+  Route::resource('pengumpulan',"PengumpulanController");
 });
+// ADMIN //
+Route::group(['middleware' => ['auth','CekRole:admin']],function(){
+    Route::resource('fakultas',"FakultasController");
+    Route::resource('lomba',"LombaController");
+    Route::group(['as' => 'jadwal.' , 'prefix' => 'jadwal'], function () {
+        Route::get('/tambah',        'JadwalController@create')->name('create')->middleware(['auth', 'CekRole:admin']);
+        Route::post('/tambah',       'JadwalController@store')->name('store')->middleware(['auth', 'CekRole:admin']);
+        Route::get('/{id}/edit',     'JadwalController@edit')->name('edit')->middleware(['auth', 'CekRole:admin']);
+        Route::patch('/{id}/edit',   'JadwalController@update')->name('update')->middleware(['auth', 'CekRole:admin']);
+        Route::delete('/{id}/hapus', 'JadwalController@destroy')->name('destroy')->middleware(['auth', 'CekRole:admin']);
+    });
+    Route::group(['as' => 'pengumuman.' , 'prefix' => 'pengumuman'], function () {
+        Route::get('/tambah',        'PengumumanController@create')->name('create')->middleware(['auth', 'CekRole:admin']);
+        Route::post('/tambah',       'PengumumanController@store')->name('store')->middleware(['auth', 'CekRole:admin']);
+        Route::get('/{id}/edit',     'PengumumanController@edit')->name('edit')->middleware(['auth', 'CekRole:admin']);
+        Route::patch('/{id}/edit',   'PengumumanController@update')->name('update')->middleware(['auth', 'CekRole:admin']);
+        Route::delete('/{id}/hapus', 'PengumumanController@destroy')->name('destroy')->middleware(['auth', 'CekRole:admin']);
+    });
+});
+// Route::resource('jadwal',"JadwalController"); //Partial
+// Route::resource('pengumuman',"PengumumanController"); //Partial
